dce65f2a59aee864663476c606c58635084b67e1
Dwight Kurt Schrute <dschrute@protonmail.com>
2021-01-12 14:12:34 -0500

    Update ig.js

--- ig.js
+++ ig.js
@@ -1,25 +1,22 @@
-
-
 const puppeteer = require('puppeteer');
 const trgtAccts = require('./targetAccounts');
 let r1 = Math.floor(Math.random() * trgtAccts.length);
 const iPhone = puppeteer.devices['iPhone X'];
+const now = new Date().toLocaleString().replace(/\//g, '.').replace(/:/g, '.').replace(', ', '_').replace(' ', '.');
+const savePath = 'C:\\o\\globaldebtsolutions.com\\gds - a\\hb\\';
 
 (async () => {
 	try {
-		const browser = await puppeteer.launch({ headless: false, args: ['--incognito'] });
+		const browser = await puppeteer.launch({ headless: true, args: ['--incognito'] });
 		const page = await browser.newPage();
 		await page.emulate(iPhone);
 		//LOGIN
 		await page.goto('https://www.instagram.com/accounts/login/?source=auth_switcher', { waitUntil: 'load' });
 		await page.waitForSelector("[name='username']");
 		await page.tap("[name='username']");
-		await page.type("[name='username']", 'onllymadisonmay');
+		await page.type("[name='username']", 'onlymadisonmay_');
 		await page.type("[name='password']", 'Lxk7zUx2VpGsqHGfc8H!');
-		await Promise.all([
-			page.waitForNavigation(),
-			page.tap("[type='submit']")
-		]);
+		await Promise.all([page.waitForNavigation(), page.tap("[type='submit']")]);
 		await page.waitForTimeout(4000);
 		//Click The NOT NOW BUTTON
 		const notifyBtn = await page.$x("//button[contains(text(), 'Not Now')]");
@@ -41,6 +38,9 @@ const iPhone = puppeteer.devices['iPhone X'];
 			console.log('posts[rndP]', posts[rndP]);
 		}
 		await page.waitForTimeout(3152);
+		await page.screenshot({ path: savePath + now + '.png', fullPage: true });
+		let postURL = await page.url();
+		console.log('postURL: ', postURL);
 		//HIT THE LIKED BY BUTTON
 		await page.tap('[href$="liked_by/"]');
 		await page.waitForTimeout(4239);
@@ -53,27 +53,24 @@ const iPhone = puppeteer.devices['iPhone X'];
 
 		let follows = await page.$x("//button[contains(text(), 'Follow')]");
 		if (follows.length > 0) {
-
 			let r72 = Math.abs(Math.floor(Math.random() * 72) + 1 - Math.floor(Math.random() * 71) + 1);
 			let r71 = Math.abs(Math.floor(Math.random() * 72) + 1 - Math.floor(Math.random() * 70) + 1);
 			let r70 = Math.abs(Math.floor(Math.random() * 72) + 1 - Math.floor(Math.random() * 69) + 1);
 			let r69 = Math.abs(Math.floor(Math.random() * 72) + 1 - Math.floor(Math.random() * 68) + 1);
 
-			await follows[r72].tap({ delay: 666 });
-			await follows[r71].tap({ delay: 555 });
-			await follows[r70].tap({ delay: 888 });
-			await follows[r69].tap({ delay: 777 });
-
+			await follows[r72].tap({ delay: 1123 });
+			await follows[r71].tap({ delay: 1091 });
+			await follows[r70].tap({ delay: 996 });
+			await follows[r69].tap({ delay: 892 });
 		} else {
 			console.log('error', r72);
 		}
-
+		//BACK AND CLOSE BROWSER
 		await page.click('svg[aria-label="Close"]');
-
 		await browser.close();
 		process.exit(1);
 	} catch (e) {
 		console.log('error = ', e);
 		process.exit(1);
-	};
-})(); 
+	}
+})();
