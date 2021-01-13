cf722b37ef1539cf61281bf3f57f76f1a1607e4b
Dwight Kurt Schrute <dschrute@protonmail.com>
2021-01-13 16:24:56 -0500

    Update targetAccounts.js

--- targetAccounts.js
+++ targetAccounts.js
@@ -1,4 +1,50 @@
-const trgtAcct = ['https://www.instagram.com/mandi_lynn_bbw/', 'https://www.instagram.com/therealnatashatosini/', 'https://www.instagram.com/jaidexoxo_/', 'https://www.instagram.com/ivykiaraofficial/', 'https://www.instagram.com/tatted.mama44/', 'https://www.instagram.com/officialmakaylaalt/', 'https://www.instagram.com/amberleighhh/', 'https://www.instagram.com/devin.meow/', 'https://www.instagram.com/jessicakes33/', 'https://www.instagram.com/jessicacakes2/', 'https://www.instagram.com/jessicafitcake/', 'https://www.instagram.com/millenawhite/', 'https://www.instagram.com/_lucky_ladybird/', 'https://www.instagram.com/millena_white/', 'https://www.instagram.com/missjessarhodes/', 'https://www.instagram.com/fierysireniv/', 'https://www.instagram.com/egoistqaa/', 'https://www.instagram.com/liubov_kalish/', 'https://www.instagram.com/lsdiamonds69/'];
+const targetAccounts = [
+	'https://www.instagram.com/thegypsynurse/',
+	'https://www.instagram.com/boandtee/',
+	'https://www.instagram.com/ohpolly/',
+	'https://www.instagram.com/ohpollyswim/',
+	'https://www.instagram.com/noireblanc.ca/',
+	'https://www.instagram.com/pallascouture/',
+	'https://www.instagram.com/pallascollective/',
+	'https://www.instagram.com/studio.albus/',
+	'https://www.instagram.com/seduction_la/',
+	'https://www.instagram.com/fuckologyofficial/',
+	'https://www.instagram.com/betchwithnolife/',
+	'https://www.instagram.com/basic/',
+	'https://www.instagram.com/bipolar_girlfriend_/',
+	'https://www.instagram.com/ohmboutique/',
+	'https://www.instagram.com/yourbreakupbestie/',
+	'https://www.instagram.com/sahirajewelrydesign/',
+	'https://www.instagram.com/okb_int/',
+	'https://www.instagram.com/cottononbody/',
+	'https://www.instagram.com/lv_perfection_medspa/',
+	'https://www.instagram.com/berrybeachyswimwear/',
+	'https://www.instagram.com/callherdaddy/',
+	'https://www.instagram.com/lovetrends.shop/',
+	'https://www.instagram.com/glossy_zodiac/',
+	'https://www.instagram.com/bossbabe.inc/',
+];
 
+module.exports = targetAccounts;
 
-module.exports = trgtAcct;
+/*LIST TARGETING MEN ON WOMENS PAGES - ONLYFANS 
+	'https://www.instagram.com/mandi_lynn_bbw/',
+	'https://www.instagram.com/therealnatashatosini/',
+	'https://www.instagram.com/jaidexoxo_/',
+	'https://www.instagram.com/ivykiaraofficial/',
+	'https://www.instagram.com/tatted.mama44/',
+	'https://www.instagram.com/officialmakaylaalt/',
+	'https://www.instagram.com/amberleighhh/',
+	'https://www.instagram.com/devin.meow/',
+	'https://www.instagram.com/jessicakes33/',
+	'https://www.instagram.com/jessicacakes2/',
+	'https://www.instagram.com/jessicafitcake/',
+	'https://www.instagram.com/millenawhite/',
+	'https://www.instagram.com/_lucky_ladybird/',
+	'https://www.instagram.com/millena_white/',
+	'https://www.instagram.com/missjessarhodes/',
+	'https://www.instagram.com/fierysireniv/',
+	'https://www.instagram.com/egoistqaa/',
+	'https://www.instagram.com/liubov_kalish/',
+	'https://www.instagram.com/lsdiamonds69/',
+	*/
