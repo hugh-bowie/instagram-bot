27dc0bd076a7effc083235d2f9f41e331b4ecb2e
Dwight Kurt Schrute <dschrute@protonmail.com>
2021-01-14 00:23:57 -0500

    Update ig.js

--- ig.js
+++ ig.js
@@ -8,64 +8,66 @@ const savePath = 'C:\\o\\globaldebtsolutions.com\\gds - a\\hb\\';
 
 (async () => {
 	try {
-		const browser = await puppeteer.launch({ headless: false, args: ['--incognito'] });
+		const browser = await puppeteer.launch({ headless: true, args: ['--incognito'] });
 		const page = await browser.newPage();
 		await page.emulate(iPhone);
+
 		//LOGIN
 		await page.goto('https://www.instagram.com/accounts/login/?source=auth_switcher', { waitUntil: 'load' });
 		await page.waitForSelector("[name='username']");
 		await page.tap("[name='username']");
-		await page.type("[name='username']", 'hb.iv', { delay: r(5, 20) });
-		await page.type("[name='password']", 'Hb24pZ26gLUiScwy0PA', { delay: r(5, 20) });
+		await page.type("[name='username']", 'hb.iv', { delay: r(50, 100) });
+		await page.type("[name='password']", 'Hb24pZ26gLUiScwy0PA', { delay: r(50, 100) });
 		await Promise.all([page.waitForNavigation(), page.tap("[type='submit']")]);
 		await page.waitForTimeout(r(3000, 5000));
+
 		//Click The NOT NOW BUTTON
 		const notifyBtn = await page.$x("//button[contains(text(), 'Not Now')]");
 		if (notifyBtn.length > 0) {
 			await notifyBtn[0].tap();
+			await page.waitForTimeout(r(3000, 5000));
 		} else {
 			console.log('no notifaction buttons to click');
 		}
-		await page.waitForTimeout(r(3000, 5000));
+
 		//GOTO RANDOM ACCOUNT FROM LIST
 		await page.goto(targetAccounts[r1]);
 		await page.waitForTimeout(r(3000, 5000));
+
 		//GET TOP 22 POSTS
 		const posts = await page.$x('//*[@sizes="123px"]');
 		if (posts.length > 0) {
-			const r22 = Math.floor(Math.random() * 22) + 1;
-			await posts[r22].tap();
+			await posts[r(1, 22)].tap();
+			await page.waitForTimeout(r(3000, 5000));
 		} else {
-			console.log('posts[rndP]', posts[rndP]);
+			console.log('posts', posts.length);
 		}
-		await page.waitForTimeout(3152);
-		await page.screenshot({ path: savePath + now + '.png', fullPage: true });
-		let postURL = await page.url();
-		console.log('postURL: ', postURL);
+
 		//HIT THE LIKED BY BUTTON
 		await page.tap('[href$="liked_by/"]');
-		await page.waitForTimeout(4239);
-		//GET 72 FOLLOWERS
-		await page.keyboard.press('PageDown', { delay: 801 });
-		await page.keyboard.press('PageDown', { delay: 659 });
-		await page.keyboard.press('PageDown', { delay: 499 });
-		await page.keyboard.press('PageDown', { delay: 719 });
-		await page.keyboard.press('PageDown', { delay: 519 });
+		await page.waitForTimeout(r(3000, 5000));
 
-		let follows = await page.$x("//button[contains(text(), 'Follow')]");
-		if (follows.length > 0) {
-			let r72 = Math.abs(Math.floor(Math.random() * 72) + 1 - Math.floor(Math.random() * 71) + 1);
-			let r71 = Math.abs(Math.floor(Math.random() * 72) + 1 - Math.floor(Math.random() * 70) + 1);
-			let r70 = Math.abs(Math.floor(Math.random() * 72) + 1 - Math.floor(Math.random() * 69) + 1);
-			let r69 = Math.abs(Math.floor(Math.random() * 72) + 1 - Math.floor(Math.random() * 68) + 1);
-
-			await follows[r72].tap({ delay: 1123 });
-			await follows[r71].tap({ delay: 1091 });
-			await follows[r70].tap({ delay: 996 });
-			await follows[r69].tap({ delay: 892 });
-		} else {
-			console.log('error', r72);
+		//GET PAGINATION 72 FOLLOWERS 
+		let i;
+		for (i = 0; i < 5; i++) {
+			await page.keyboard.press('PageDown');
+			await page.waitForTimeout(r(3000, 5000));
 		}
+
+		//TAP FOLLOW BUTTON FOR THESE FOOLS
+		const follows = await page.$x("//button[contains(text(), 'Follow')]");
+		let z;
+		if (follows.length > 0) {
+			for (z = 0; z < 4; z++) {
+				let num = r(1, 72 - z);
+				await follows[num].tap();
+				await page.waitForTimeout(r(3000, 5000));
+			}
+		} else {
+			console.log('error follows:', follows[num]);
+		}
+		await page.screenshot({ path: savePath + now + '.png', fullPage: true });
+
 		//BACK AND CLOSE BROWSER
 		await page.click('svg[aria-label="Close"]');
 		await browser.close();
