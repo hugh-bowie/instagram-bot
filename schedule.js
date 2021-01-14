30e044c67f733b3498e23c37e479a5d27f4c64e4
Dwight Kurt Schrute <dschrute@protonmail.com>
2021-01-14 11:54:46 -0500

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,7 +1,8 @@
 const spawn = require('child_process').spawn;
 const schedule = require('node-schedule');
 
-schedule.scheduleJob('*/35 * * * *', () => {
-	console.log('running ig.js every 35 min');
+schedule.scheduleJob('13,43 * * * *', () => {
+	let now = new Date().toLocaleString();
+	console.log('running ig.js every 35 min' + now);
 	spawn('node', ['ig.js']);
 });
