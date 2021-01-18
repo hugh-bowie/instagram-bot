733c70ad5c6492065b6b29f557aca94f6565b6ea
Dwight Kurt Schrute <dschrute@protonmail.com>
2021-01-18 08:51:31 -0500

    Update device.js

--- device.js
+++ device.js
@@ -1,14 +1,15 @@
 const device = {
-	name: 'iPhone 11 Pro Max',
-	userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1',
-	viewport: {
-		width: 414,
-		height: 896,
-		deviceScaleFactor: 3,
-		isMobile: true,
-		hasTouch: true,
-		isLandscape: false,
-	},
-};
+  name: 'iPhone 11 Pro Max',
+  userAgent:
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1',
+  viewport: {
+    width: 414,
+    height: 896,
+    deviceScaleFactor: 3,
+    isMobile: true,
+    hasTouch: true,
+    isLandscape: false,
+  }
+}
 
-module.expoorts = device;
+module.exports = device;
