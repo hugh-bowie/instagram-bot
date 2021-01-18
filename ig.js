2b21b526ba80527a115addfc7fa0342c30e13953
Dwight Kurt Schrute <dschrute@protonmail.com>
2021-01-18 08:50:42 -0500

    Update ig.js

--- ig.js
+++ ig.js
@@ -3,6 +3,7 @@ const device = require('./device');
 const targetAccounts = require('./targetAccounts');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 let r = require('./r');
+const r35 = r(3000, 5000);
 let r1 = Math.floor(Math.random() * targetAccounts.length);
 const now = new Date().toLocaleString().replace(/\//g, '.').replace(/:/g, '.').replace(', ', '_').replace(' ', '.');
 const savePath = 'C:\\o\\globaldebtsolutions.com\\gds - a\\hb\\';
@@ -11,47 +12,47 @@ puppeteer.use(StealthPlugin());
 (async () => {
 	try {
 		//----initialize
-		const browser = await puppeteer.launch({ headless: false, args: ['--incognito'] });
+		const browser = await puppeteer.launch({ headless: true, args: ['--incognito'] });
 		const page = await browser.newPage();
 		await page.emulate(device);
-		await page.goto('https://www.instagram.com/accounts/login/?source=auth_switcher', { waitUntil: 'load' });
-		/*//----accept_cookies
-								const cookieBtn = await page.$x("//button[contains(text(), 'Accept')]");
-								if (cookieBtn.length > 0) {
-									await cookieBtn[0].tap();
-									await page.waitForTimeout(r(1000, 2000));
-								} else {
-									console.log('no notifaction buttons to click');
-								}*/
 		//----login
+		await page.goto('https://www.instagram.com/accounts/login/?source=auth_switcher', { waitUntil: 'load' });
+		//----accept_cookies
+		/*const cookieBtn = await page.$x("//button[contains(text(), 'Accept')]");
+		if (cookieBtn.length > 0) {
+			await cookieBtn[0].tap();
+			await page.waitForTimeout(r(1000, 2000));
+		} else {
+			console.log('no notifaction buttons to click');
+		}*/
 		await page.waitForSelector("[name='username']");
 		await page.tap("[name='username']");
 		await page.type("[name='username']", 'hb.iv', { delay: r(50, 100) });
 		await page.type("[name='password']", 'Hb24pZ26gLUiScwy0Pa', { delay: r(50, 100) });
 		await Promise.all([page.waitForNavigation(), page.tap("[type='submit']")]);
-		await page.waitForTimeout(r(2000, 4000));
+		await page.waitForTimeout(r35);
 		//----notifications
 		const notifyBtn = await page.$x('//button[contains(text(), "Not Now")]');
 		if (notifyBtn.length > 0) {
 			await notifyBtn[0].tap();
-			await page.waitForTimeout(r(2000, 4000));
+			await page.waitForTimeout(r35);
 		} else {
 			console.log('no notifaction buttons to click');
 		}
 		//----target_account
 		await page.goto(targetAccounts[r1]);
-		await page.waitForTimeout(r(2000, 5000));
+		await page.waitForTimeout(r35);
 		//----all_account_posts
 		const posts = await page.$x('//*[@sizes="136px"]');
 		if (posts.length > 0) {
 			await posts[r(1, 22)].tap();
-			await page.waitForTimeout(r(2000, 5000));
+			await page.waitForTimeout(r35);
 		} else {
 			console.log('posts', posts.length);
 		}
 		//----liked_by
 		await page.tap('[href$="liked_by/"]');
-		await page.waitForTimeout(r(2000, 5000));
+		await page.waitForTimeout(r35);
 		//----pagination_72_followers 
 		let i;
 		for (i = 0; i < 5; i++) {
@@ -59,20 +60,20 @@ puppeteer.use(StealthPlugin());
 			await page.waitForTimeout(r(1000, 2000));
 		}
 		//DYNAMIC CRAWL EACH FOLLOWER
-		let likers = await page.$x('//div[@id]');
+		let likers = await page.$$eval('a[title]', lis => lis.map(li => li.getAttribute('href')));
 		let x;
-		let y = r(4, 7);
+		let y = r(8, 10);
 		if (likers.length > 0) {
-			for (x = 0; x < y; x++) {
-				let num = r(1, 72 - y);
-				await likers[num].tap();//---------------------------------------------click a random liker
-				await page.waitForTimeout(r(3000, 5000));
-				//----get users posts----
-				let posts = await page.$x('//*[@sizes="136px"]');
-				if (posts.length > 0) {
+			for (x = 0; x < y; x++) {//------------------------------------------------repeat 4,7 times
+				let num = r(0, likers.length);
+				await page.goto('https://www.instagram.com' + likers[num]);//----------goto a random link
+				await page.waitForTimeout(r(4000, 5000));
+				await page.waitForSelector('#react-root');
+				let posts = await page.$x('//*[@sizes="136px"]');//--------------------get users top 24 posts
+				if (posts.length > 0) {//----------------------------------------------if users posts are public
 					let p = r(0, posts.length);
 					await posts[p].tap();//--------------------------------------------click random post to like
-					await page.waitForTimeout(r(3000, 5000));
+					await page.waitForTimeout(r35);
 					//----get all the like buttons----
 					let like = await page.$x('//*[@aria-label="Like"]');
 					if (like.length > 0) {
@@ -81,26 +82,22 @@ puppeteer.use(StealthPlugin());
 					} else {
 						console.log('like btn selector not found');
 					}
-				} else {//-------------------------------------------------------------private profile or no posts
+				} else {//-------------------------------------------------------------if users posts are private
 					let follow = await page.$x("//button[contains(text(), 'Follow')]");
 					if (follow.length > 0) {
 						await follow[0].tap();
 						await page.waitForTimeout(r(2000, 3000));
-						await page.tap('[aria-label="Back"]');//-----------------------followed
-						await page.waitForTimeout(r(2000, 4000));
 					}
 				}
 			}
 		}
 
-		await page.screenshot({ path: savePath + now + '.png', fullPage: true });
-
-		//BACK AND CLOSE BROWSER
-		//await browser.close();
-		//process.exit(1);
+		//BACK AND CLOSE BROWSER*/
+		await browser.close();
+		process.exit(1);
 	} catch (e) {
 		console.log('error = ', e);
-		//process.exit(1);
+		process.exit(1);
 	}
 })();
 
