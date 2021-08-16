614d1c88cd4ae73ce3ad627d2db1d604280fe862
hugh-bowie <hughbowie@me.com>
2021-08-15 21:11:39 -0400

    any

--- schedule.js
+++ schedule.js
@@ -1,7 +1,7 @@
 const spawn = require('child_process').spawn;
 const schedule = require('node-schedule');
 
-schedule.scheduleJob('13,43 * * * *', () => {
+schedule.scheduleJob('17 * * * *', () => {
 	let now = new Date().toLocaleString();
 	console.log('running ig.js every hour at :13 and :43 ' + now);
 	spawn('node', ['ig.js']);
