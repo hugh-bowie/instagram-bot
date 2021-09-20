70b9772113b82e490a5f14c94fb8a73fb094c1a1
hugh-bowie <hughbowie@me.com>
2021-09-20 09:04:48 -0400

    a

--- schedule.js
+++ schedule.js
@@ -4,7 +4,7 @@ const schedule = require('node-schedule');
 const { r, log, timeNow } = require('./src/helpers');
 const firstRun = r(15, 20);
 const secondRun = r(43, 48);
-schedule.scheduleJob(' 11,46 * * * *', () => {
+schedule.scheduleJob(' 33 * * * *', () => {
 	let now = new Date().toLocaleString();
 	console.log(now + ' running ig.js every hour at 15 and 36 ');
 	spawn('node', ['test.js']);
