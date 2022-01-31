8f260caea9dd440882a932e8868569aff8ea16f1
hughbowie <hughbowie@me.com>
2022-01-31 16:31:29 -0500

    any

--- dks.js
+++ dks.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
-const { r, log, logD, device, badAccounts, r15 } = require('./src/helpers');
+const { r, log, logD, device, badAccounts, r15, r23 } = require('./src/helpers');
 const { memeAccounts } = require('./src/meme');
 
 (async () => {
   try {
 
     //----initialize
-    const browser = await puppeteer.launch({ headless: false, args: ['--incognito'] }); //////// slowMo: 100,
+    const browser = await puppeteer.launch({ headless: true, args: ['--incognito'] }); //////// slowMo: 100,♻♻♻♻♻♻♻♻♻♻
     const page = await browser.newPage();
     await page.emulate(device);
 
@@ -26,7 +26,7 @@ const { memeAccounts } = require('./src/meme');
     const notifyBtn = await page.$x('//*[contains(text(), "Not Now")]');
     if (notifyBtn) {
       await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle2' }), notifyBtn[0].tap()]);
-      await page.waitForTimeout(r15);
+      await page.waitForTimeout(r23);
     }
 
     //---- got to home and screenshot the follower count
@@ -47,9 +47,9 @@ const { memeAccounts } = require('./src/meme');
     await page.goto(farmAccount, { waitUntil: 'networkidle2' });
     log(`Farming this Account: ${farmAccount}`);
     await page.keyboard.press('PageDown');
-    await page.waitForTimeout(r15);
+    await page.waitForTimeout(r23);
     await page.keyboard.press('PageDown');
-    await page.waitForTimeout(r15);
+    await page.waitForTimeout(r23);
 
     //----goto one random post
     let postHrefs = await page.$$eval('a[href^="/p/"]', href => href.map(hre => hre.getAttribute('href')));
@@ -57,14 +57,14 @@ const { memeAccounts } = require('./src/meme');
       let rPost = r(1, postHrefs.length);
       await page.goto('https://www.instagram.com' + postHrefs[rPost], { waitUntil: 'networkidle2' });
       log(`Targeting users who liked post number ${rPost}  ` + (await page.url()));
-      await page.waitForTimeout(r15);
+      await page.waitForTimeout(r23);
     }
 
     //----click the Likes number on the photo
     let likedByBtn = await page.$('[href$="liked_by/"]'); // $x('//*[contains(@href, "/liked_by/")]')
     if (likedByBtn) {
       await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle2' }), page.tap('[href$="liked_by/"]')]);
-      await page.waitForTimeout(r15);
+      await page.waitForTimeout(r23);
       await page.waitForSelector('h1', { visible: true });
     }
 
@@ -87,7 +87,7 @@ const { memeAccounts } = require('./src/meme');
     if (publicHrefs) {
       for (let x = 0; x < rNum; x++) {
         await page.goto('https://www.instagram.com' + publicHrefs[x], { waitUntil: 'networkidle2' }); //>>>>>>>> USER WITH ZERO POSTS >>>>>'https://www.instagram.com/jasminee.hampton/'
-        await page.waitForTimeout(r15);
+        await page.waitForTimeout(r23);
         await page.waitForSelector('h1', { visible: true });
         let currentURL = await page.url();
         let searchBool = badAccounts.includes(currentURL);
@@ -103,10 +103,10 @@ const { memeAccounts } = require('./src/meme');
               let closeBtn = await page.$('[aria-label="Close"]');
               if (closeBtn.length === 1) {
                 await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle2' }), page.tap('[aria-label="Close"]')]);
-                await page.waitForTimeout(r15);
+                await page.waitForTimeout(r23);
               } else {
                 await page.goBack({ waitUntil: 'networkidle2' }); // >>>>>>>> USER WITH ZERO POSTS >>>>>'https://www.instagram.com/jasminee.hampton/'
-                await page.waitForTimeout(r15);
+                await page.waitForTimeout(r23);
               }
               //----- get top 24 posts
               let posts = await page.$$eval('a[href^="/p/"]', hrefs => hrefs.map(ref => ref.getAttribute('href')));// let posts = await page.$x('//*[@class="FFVAD"]');
@@ -115,7 +115,7 @@ const { memeAccounts } = require('./src/meme');
                 let p = r(0, posts.length);
                 //----click One random Public post to like
                 await page.goto('https://www.instagram.com' + posts[p], { waitUntil: 'networkidle2' });
-                await page.waitForTimeout(r15);
+                await page.waitForTimeout(r23);
                 await page.waitForSelector('svg[aria-label="More options"]');
                 //----the Like button to hit // await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle2' }), posts[p].tap()]);
                 let likeBtn = await page.$x('//*[@aria-label="Like"]');
@@ -128,13 +128,13 @@ const { memeAccounts } = require('./src/meme');
                   //add comment method one
                   // const commentURL = (await page.url()) + 'comments/';
                   // await page.goto(commentURL, { waitUntil: 'networkidle2' });
-                  // await page.waitForTimeout(r15);
+                  // await page.waitForTimeout(r23);
                   // await page.tap('textarea.Ypffh');
-                  // await page.waitForTimeout(r15);
+                  // await page.waitForTimeout(r23);
                   // const thisComment = memeComments[r(0, memeComments.length)];
                   // logD(`			✎ Comment: ${thisComment}\n`);
                   // await page.type('textarea.Ypffh', thisComment);
-                  // await page.waitForTimeout(r15);
+                  // await page.waitForTimeout(r23);
                   // const postBTN = await page.$x('//button[contains(text(), "Post")]');
                   // if (postBTN) {
                   // 	await postBTN[0].tap();
