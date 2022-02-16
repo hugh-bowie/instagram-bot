7abd5bedf104155fe2710f7d135967d146573af7
hughbowie <hughbowie@me.com>
2022-02-16 09:40:53 -0500

    any

--- src/helpers.js
+++ src/helpers.js
@@ -56,7 +56,7 @@ function logT(data) {
 // pretends this is a phone not a desktop
 const device = {
 	name: 'iPhone 13 Pro Max',
-	userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Mobile/15E148 Safari/604.1',
+	userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Mobile/15E148 Safari/604.1',
 	viewport: {
 		width: 428,
 		height: 926,
